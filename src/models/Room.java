81dfca7a4fb9dc424d018036266b7947ec820127
MACHADO-Caetano <machadocaetano@rede.ulbra.br>
2025-06-11 21:22:40 -0300

    Pega aí anderson

---
+++ src/models/Room.java
@@ -0,0 +1,16 @@
+public class Room {
+    private int roomNumber;
+    private String roomType;
+    private String roomDescription;
+    public Room next;
+    public Room previous;
+
+    public Room(int roomNumber, String roomType, String roomDescription) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.roomDescription = roomDescription;
+        this.next = null;
+        this.previous = null;
+    }
+
+};
