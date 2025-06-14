93e39816fc600efbc33d2cdf255971389e99447c
MACHADO-Caetano <machadocaetano@rede.ulbra.br>
2025-06-13 21:06:50 -0300

    Funções principais feitas, finalizar

--- src/models/Labirinth.java
+++ src/models/Labirinth.java
@@ -2,28 +2,76 @@ package models;
 
 public class Labirinth {
 
-    private Room[] room;
-
-
-
-    public class RoomPointer{
+    public class RoomKnot<Room>{
         public Room room;
-        public RoomPointer next;
+        public RoomKnot previous;
+        public RoomKnot next;
 
-        public RoomPointer(Room room){
+        public RoomKnot(Room room){
             this.room = room;
+            this.previous = null;
             this.next = null;
-
         }
     }
 
-    private RoomPointer startLabirinth;
+    private RoomKnot startLabirinth;
+    private RoomKnot endLabirinth;
 
     public Labirinth(){
         this.startLabirinth = null;
+        this.endLabirinth = null;
+        //implementar validação para que o labirinto não tenha menos que 10 salas, creio que seja necessário criar aqui
     }
 
-    public void createRooms(Room room){
-
+    public void createRoom(Room room) {
+        RoomKnot<Room> newKnot = new RoomKnot<>(room);
+        if (startLabirinth == null) {
+        startLabirinth = newKnot;
+        endLabirinth = newKnot;
+        } else {
+        endLabirinth.next = newKnot;
+        newKnot.previous = endLabirinth;
+        endLabirinth = newKnot;
+        }
     }
+
+    public void showRooms() {
+        if (startLabirinth == null) {
+            System.out.println("Não há salas para visualização");
+            return;
+        }
+    
+        RoomKnot current = startLabirinth;
+        while (current != null) {
+            System.out.print(current.room + " ");
+            current = current.next;
+        }
+        System.out.println();
+    }
+
+    //ou fazer uma função para validar o "tamanho" do labirinto à parte
+
+    public void removeRoom(int roomNumber) {
+        if (startLabirinth == null) return;
+
+        RoomKnot<Room> current = startLabirinth;
+        while (current != null) {
+            if (current.room.roomNumber == roomNumber) {
+                if (current == startLabirinth) {
+                    startLabirinth = current.next;
+                    if (startLabirinth != null) startLabirinth.previous = null;
+                } 
+				else if (current == endLabirinth) {
+                    endLabirinth = current.previous;
+                    if (endLabirinth != null) endLabirinth.next = null;
+                } else {
+                    current.previous.next = current.next;
+                    current.next.previous = current.previous;
+                }
+                return;
+            }
+            current = current.next;
+        }
+    }
+
 }
--- src/models/Room.java
+++ src/models/Room.java
@@ -1,12 +1,13 @@
 package models;
 
 public class Room {
-    private int roomNumber;
-    private String roomType;
-    private String roomDescription;
+    public int roomNumber;
+    public String roomType;
+    public String roomDescription;
     public Room next;
     public Room previous;
 
+    
     public Room(int roomNumber, String roomType, String roomDescription) {
         this.roomNumber = roomNumber;
         this.roomType = roomType;
