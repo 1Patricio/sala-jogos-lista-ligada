d3994392cce683b897823d70cf29a24dedd52b89
MACHADO-Caetano <machadocaetano@rede.ulbra.br>
2025-06-19 13:54:52 -0300

    Primeiros testes com menu funcionais,continuar desenvolvendo lore do jogo

--- src/App.java
+++ src/App.java
@@ -1,3 +1,6 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 import java.util.Scanner;
 
 import models.Labirinth;
@@ -10,18 +13,19 @@ public class App {
 
         Labirinth labirinth = new Labirinth();
         // // Lista de salas
-        Room room0 = new Room(0, TypeRoom.Normal, "Sala central do labirinto");
-        Room room1 = new Room(1, TypeRoom.Normal, "Entrada escura de pedra úmida.");
-        Room room2 = new Room(2, TypeRoom.Trap, "Chão falso com espinhos escondidos.");
-        Room room3 = new Room(3, TypeRoom.Normal, "Corredor com tochas apagadas.");
-        Room room4 = new Room(4, TypeRoom.Award, "Sala com um baú de ouro reluzente.");
-        Room room5 = new Room(5, TypeRoom.Trap, "Armadilha de flechas nas paredes.");
-        Room room6 = new Room(6, TypeRoom.Normal, "Passagem estreita cheia de teias de aranha.");
-        Room room7 = new Room(7, TypeRoom.Award, "Fonte mágica que restaura a vida.");
-        Room room8 = new Room(8, TypeRoom.Trap, "Estátuas que disparam laser quando ativadas.");
-        Room room9 = new Room(9, TypeRoom.Exit, "Porta rúnica que leva à saída da dungeon.");
+        Room room1 = new Room(0, TypeRoom.Normal, "Sala central do labirinto");
+        Room room2 = new Room(1, TypeRoom.Normal, "Entrada escura de pedra úmida.");
+        Room room3 = new Room(2, TypeRoom.Trap, "Chão falso com espinhos escondidos.");
+        Room room4 = new Room(3, TypeRoom.Normal, "Corredor com tochas apagadas.");
+        Room room5 = new Room(4, TypeRoom.Award, "Sala com um baú de ouro reluzente.");
+        Room room6 = new Room(5, TypeRoom.Trap, "Armadilha de flechas nas paredes.");
+        Room room7 = new Room(6, TypeRoom.Normal, "Passagem estreita cheia de teias de aranha.");
+        Room room8 = new Room(7, TypeRoom.Award, "Fonte mágica que restaura a vida.");
+        Room room9 = new Room(8, TypeRoom.Trap, "Estátuas que disparam laser quando ativadas.");
+        Room room10 = new Room(9, TypeRoom.Exit, "Porta rúnica que leva à saída da dungeon.");
 
-        labirinth.createRoom(room0);
+        List<Room> listRooms = new ArrayList<>();
+
         labirinth.createRoom(room1);
         labirinth.createRoom(room2);
         labirinth.createRoom(room3);
@@ -31,19 +35,26 @@ public class App {
         labirinth.createRoom(room7);
         labirinth.createRoom(room8);
         labirinth.createRoom(room9);
+        labirinth.createRoom(room10);
 
-        // player.changingRoom(room1);
-        // player.getNameAndRoom();
-
-        // player.changingRoom(room2);
-        // player.win();
+        listRooms.add(room1);
+        listRooms.add(room2);
+        listRooms.add(room3);
+        listRooms.add(room4);
+        listRooms.add(room5);
+        listRooms.add(room6);
+        listRooms.add(room7);
+        listRooms.add(room8);
+        listRooms.add(room9);
+        listRooms.add(room10);
 
         Scanner scanner = new Scanner(System.in);
-        Player player = new Player(null, room0);
+        Player player = new Player(null, null);
+
 
         System.out.println("____Desafio do Labirinto____");
         System.out.println("1 - Iniciar");
-        System.out.println("2 - Sair");
+        System.out.println("0 - Sair");
         int option = scanner.nextInt();
         boolean game = (option == 1) ? true : false;
 
@@ -51,8 +62,9 @@ public class App {
             System.out.println("Start Game");
             String mensageName = (player.name == null) ? "1 - Inserir nome do jogador" : "1 - Editar nome do jogador";
             System.out.println(mensageName);
-            System.out.println("2 - Mudar de Sala");
-            System.out.println("9 - Sair");
+            String enterLabirinth = (player.room == null) ? "2 - Entrar no labirinto" : "2 - Encontrar sala";
+            System.out.println(enterLabirinth);
+            System.out.println("0 - Sair");
             int optionGame = scanner.nextInt();
 
             switch (optionGame) {
@@ -62,10 +74,38 @@ public class App {
                     player.name = name;
                     break;
                 case 2:
-                    player.changingRoom(labirinth, room2);
+                    if(player.room == null){
+                        System.out.println(player.name + " está entrando no labirinto . . .");
+                        System.out.println(player.name + " encontra a primeira sala do labirinto, e tem que tomar uma decisão:");
+                        System.out.println("____________________________");
+                        System.out.println("E - Entrar na sala");
+                        System.out.println("A - Avançar para próxima sala");
+                        System.out.println("____________________________");
+                        String choose = scanner.next();
+
+                        switch (choose.toUpperCase()) {
+                            case "E":
+                                System.out.println(player.name + " entrou na sala " + room1.roomDescription + " por agora, está seguro, mas ainda há mais para progredir...");
+                                player.room = room1;
+                                
+                                break;
+                            case "A":
+                                System.out.println(player.name + " avança para a próxima sala, adentrando mais o labirinto");
+                                player.changingRoom(labirinth, chooserRooms(listRooms));
+                                break;
+                            default:
+                                break;
+                        }
+
+                    }
+                    System.out.println("Ao adentrar mais a sala " + player.name + "encontra uma nova porta, e tem que tomar uma decisão:");
+
+
+                    
                     break;
-                case 9:
+                case 0:
                     game = false;
+                    System.out.println("Obrigado por jogar!");
                     break;
                 default:
                     System.out.println("Opção inválida. Tente novamente.");
@@ -74,5 +114,14 @@ public class App {
         }
         scanner.close();
 
+    }
+
+    public static Room chooserRooms(List<Room> listRooms){
+
+        Random random = new Random();
+
+        int chooser = random.nextInt(listRooms.size());
+        
+        return listRooms.get(chooser);
     }
 }
